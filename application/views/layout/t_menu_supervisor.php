ea742864ff6d4ec05adf76f1ef83411242a18378
sopian sopiana iskandar <sopianaiskandar@yahoo.co.id>
2013-01-30 20:35:19 +0700

    v2 - update

---
+++ application/views/layout/t_menu_supervisor.php
@@ -0,0 +1,18 @@
+
+
+<div id="menu_supervisor">
+	
+    <div id="submenu_supervisor">
+    Pengelolaan Jadwal
+    </div>
+    
+    <div id="submenu_supervisor">
+    Pengelolaan Sales
+    </div>
+    
+    <div id="submenu_supervisor">
+    Lihat Laporan Harian
+    </div>
+
+</div>
+
---
+++ application/views/layout/temp_spvsr.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="<?php base_url() ?>css/style.css">
+</head>
+<body>
+<div id="wrapper">
+	
+        <div id="header">
+        <?php
+            $this->load->view('layout/t_header');
+        ?>
+        </div>  
+        <div id="menu_super">
+        <?php
+            $this->load->view('layout/t_menu_supervisor');
+        ?>
+            
+        </div>
+          
+        <div id="content">
+        <?php 
+			$this->load->view('layout/t_content');
+		?>
+        </div>
+        <?php
+			$this->load->view('layout/t_footer');
+		?>
+        <div id="footer">
+        
+    </div>
+    
+</div>   
+</body>
+</html>
