a763c2d46210cf67491ddd183af6118af56cec86
sopian sopiana iskandar <sopianaiskandar@yahoo.co.id>
2013-01-30 19:46:09 +0700

    v1-update

--- application/views/layout/template.php
+++ application/views/layout/template.php
@@ -1,12 +1,27 @@
 <html>
 <head>
-<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/syle.css">
+<link rel="stylesheet" type="text/css" href="<?php base_url() ?>css/style.css">
 </head>
 <body>
-	<div id="#header">
-    <?php
-		$this->load->view('layout/t_header');
-	?>
+<div id="wrapper">
+	
+        <div id="header">
+        <?php
+            $this->load->view('layout/t_header');
+        ?>
+        </div>    
+        <div id="content">
+        <?php 
+			$this->load->view('layout/t_content');
+		?>
+        </div>
+        <?php
+			$this->load->view('layout/t_footer');
+		?>
+        <div id="footer">
+        
     </div>
+    
+</div>   
 </body>
 </html>
