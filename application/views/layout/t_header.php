0d68fb564d3f1ec9bd99a1a1e7dbcaa0a2956276
sopian sopiana iskandar <sopianaiskandar@yahoo.co.id>
2013-01-30 19:15:56 +0700

    init

---
+++ application/views/layout/t_header.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+
+</head>
+
+<body>
+
+<h1>
+esales
+</h1>
+
+</body>
+
+</html>
---
+++ application/views/layout/template.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>css/syle.css">
+</head>
+<body>
+	<div id="#header">
+    <?php
+		$this->load->view('layout/t_header');
+	?>
+    </div>
+</body>
+</html>
